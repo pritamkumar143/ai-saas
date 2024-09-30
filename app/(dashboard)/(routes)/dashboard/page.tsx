5c938900c3cecc57550c4dc80b0553b1668b4609
Pritam Kumar <pritamkunar13@gmail.com>
2024-09-30 15:00:54 +0530

    authentication via clerk

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+
+const DashboardPage = () => {
+  return (
+    <>
+      <p>Dashboard (Protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </>
+  )
+}
+
+export default DashboardPage 
